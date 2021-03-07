f1b51816d3ca521acabb133d1311e99da5315613
Zulfiqar Ali <zulfiqarpk92@gmail.com>
2021-03-07 22:57:34 +0500

    prerendering

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { render } from 'react-snapshot';
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+render(
+  <App />,
+  document.getElementById('root')
+);
